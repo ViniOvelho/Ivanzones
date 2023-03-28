bd81c76fc686ba3720c6572151719edd4a907d12
ViniOvelho <125364832+ViniOvelho@users.noreply.github.com>
2023-03-28 08:45:31 -0300

    Add files via upload

---
+++ aula11/Exercicio/login.php
@@ -0,0 +1,15 @@
+
+<?php
+
+$usuario = $_GET["usuario"];
+$senha = $_GET["senha"];
+
+if($usuario == 'Yudi Tamashiro' && $senha == '40028922'){
+    echo "ta certo meu querido";
+}else{
+    echo "Errou!!!!!!!!!!!!!!!!";
+     }
+     
+
+   
+?>
