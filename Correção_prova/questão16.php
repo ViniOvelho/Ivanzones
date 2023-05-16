098a554bdd474172db2a82fe1a6e877c612db0db
ViniOvelho <125364832+ViniOvelho@users.noreply.github.com>
2023-05-16 07:01:21 -0300

    Add files via upload

---
+++ Correção_prova/questão16.php
@@ -0,0 +1,19 @@
+<?php
+
+if(isset($_GET['n1']) && isset($_GET['n2']));{
+$n1 = $_GET['n1']; 
+$n21 = $_GET['n2']; 
+
+
+if($n1 > $n2){
+    echo "$n1 é o maior valor";
+}else if ($n2 > $n1){
+    echo "$n2 é o maior valor";    
+}else{
+    echo "Os valores são iguais"
+}
+
+}else{
+    echo "impossivel prosseguir";
+}
+?>
